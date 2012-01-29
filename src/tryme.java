cc584a0ab60050ce2880f4a6729adee74ce159ba
aoun aoun <root07@gmail.com>
2012-01-30 02:50:15 +0700

    fgdgdf

---
+++ src/tryme.java
@@ -0,0 +1,17 @@
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+public class tryme {
+
+	
+	public static void main(String[] args){
+		System.out.println("Therooooot");
+		Scanner sc = new Scanner(new InputStreamReader(System.in));
+		String i = sc.next();
+		System.out.println(i);
+		
+		
+		
+	}
+}
